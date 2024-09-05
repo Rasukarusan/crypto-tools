cb50240db4dc03effcf9145043a9d1f91b8b8f1d
Rasukarusan <7gta0you1tube000@gmail.com>
2024-09-05 18:02:04 +0900

    fix lint

--- app/components/Graph.tsx
+++ app/components/Graph.tsx
@@ -1,6 +1,14 @@
 'use client'
-import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
 
 const data = [
   {
@@ -45,30 +53,35 @@ const data = [
     pv: 4300,
     amt: 2100,
   },
-];
+]
 
 export const Graph = () => {
   return (
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
-      </ResponsiveContainer>
-    );
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="pv"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
+    </ResponsiveContainer>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import {Graph} from "./components/Graph";
+import { Graph } from './components/Graph'
 
 export default function Page() {
   return (
-    <div style={{width: '100%'}}>
+    <div style={{ width: '100%' }}>
       <h1>Hello, Next.js!</h1>
-    <div style={{height: '500px'}}>
-      <Graph />
-    </div>
+      <div style={{ height: '500px' }}>
+        <Graph />
+      </div>
     </div>
   )
 }
