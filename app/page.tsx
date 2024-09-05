db49b1df0e98c19597da545bafc13354bfd7bb85
Rasukarusan <7gta0you1tube000@gmail.com>
2024-09-05 17:42:56 +0900

    recharts導入

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import {Graph} from "./components/Graph";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+    <div style={{width: '100%'}}>
+      <h1>Hello, Next.js!</h1>
+    <div style={{height: '500px'}}>
+      <Graph />
+    </div>
+    </div>
+  )
 }
