e9a4362e62b0cf278c301df695bb80b9bb043139
Rasukarusan <7gta0you1tube000@gmail.com>
2024-09-08 07:02:14 +0900

    /api/binance/symbolsを実装

---
+++ app/api/binance/symbols/route.ts
@@ -0,0 +1,19 @@
+import { type NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const currency = searchParams.get('currency') ?? ''
+
+  try {
+    const url = 'https://api.binance.com/api/v3/exchangeInfo'
+    const res = await fetch(url).then((res) => res.json())
+    const symbols = res.symbols
+      .map((symbol) => symbol.symbol)
+      .filter((v) => v.includes(currency))
+    console.log(symbols)
+    return NextResponse.json({ result: true, symbols })
+  } catch (e) {
+    console.error(e)
+    return NextResponse.json({ result: false })
+  }
+}
