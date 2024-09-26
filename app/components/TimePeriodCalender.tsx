9045efe280d405333f83a6918671e451ac649bb3
Rasukarusan <7gta0you1tube000@gmail.com>
2024-09-26 21:13:18 +0900

    期間指定した際レスポンスに収まるようなintervalを指定するようにした

--- app/components/TimePeriodCalender.tsx
+++ app/components/TimePeriodCalender.tsx
@@ -23,6 +23,31 @@ export const TimePeriodCalendar: React.FC = () => {
     )
   })
 
+  const getInterval = (startTime, endTime) => {
+    // BinanceAPIで許可されているinterval
+    const intervals = {
+      '15m': 900,
+      '30m': 1800,
+      '1h': 3600,
+      '2h': 7200,
+      '4h': 14400,
+      '6h': 21600,
+      '8h': 28800,
+      '12h': 43200,
+      '1d': 86400,
+      '3d': 259200,
+      '1w': 604800,
+      '1M': 2592000,
+    }
+    const durationInSeconds = Math.abs(dayjs(endTime).diff(startTime, 'second'))
+    for (const key of Object.keys(intervals)) {
+      if (durationInSeconds / intervals[key] <= 500) {
+        return key
+      }
+    }
+    return '15m'
+  }
+
   return (
     <div className="flex items-center justify-center">
       <DatePicker
@@ -32,6 +57,7 @@ export const TimePeriodCalendar: React.FC = () => {
           setStartDate(date)
           setSearchParams({
             ...searchParams,
+            interval: getInterval(date, endDate),
             startTime: dayjs(date).format('YYYY-MM-DD 00:00:00'),
           })
         }}
@@ -45,6 +71,7 @@ export const TimePeriodCalendar: React.FC = () => {
           setEndDate(date)
           setSearchParams({
             ...searchParams,
+            interval: getInterval(startDate, date),
             endTime: dayjs(date).add(1, 'day').format('YYYY-MM-DD 00:00:00'),
           })
         }}
